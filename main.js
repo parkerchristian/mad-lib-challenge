b0e589f86366d64d3bc2fe70cda9cc5c9e0bc5b0
Parker Helland <hellandp@gmail.com>
2019-01-24 23:12:13 -0800

    thank you next

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ function madLib() {
     var descriptiveAdjective = descriptiveAdjectiveInput.value;
     var slangWord = slangWordInput.value;
 
-    var results = famousPerson + ' was stoked about the MadLib Challenge in class today. But they were having trouble coming up with ' + anyNumber + ' ' + pluralNoun + ' to help them with the Challenge. They figured out they should ' + verb + ' to save time. Little did they know it caused them to ' + descriptiveAdjective + '. But in the end it^s all ' + slangWord + ' and they were really happy about that.'; 
+    var results = `<span style="text-decoration:underline ; font-weight:bold"> ${famousPerson}</span> was stoked about the MadLib Challenge in class today. But they were having trouble coming up with <span style="text-decoration:underline ; font-weight:bold"> ${anyNumber}</span> <span style="text-decoration:underline ; font-weight:bold"> ${pluralNoun}</span> to help them with the Challenge. They figured out they should <span style="text-decoration:underline ; font-weight:bold"> ${verb}</span> to save time. Little did they know it caused them to <span style="text-decoration:underline ; font-weight:bold"> ${descriptiveAdjective}</span>. But in the end it\'s all <span style="text-decoration:underline ; font-weight:bold"> ${slangWord}</span> and they were really happy about that.`; 
 
     var theSentence = document.getElementById('theSentence');
-    theSentence.textContent = results;
+    theSentence.innerHTML = results;
     
 
 
