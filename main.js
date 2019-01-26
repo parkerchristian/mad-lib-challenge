5b4e3a48424e0c3bff53123779462d130188c047
Parker Helland <hellandp@gmail.com>
2019-01-26 13:44:08 -0800

    ok

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//mad lib button
 function madLib() {
     
     //creates new variables to call inputs
@@ -19,24 +20,42 @@ function madLib() {
     //madlib sentence with input variables in order to complete madlib
     var results = `<span style="text-decoration:underline ; font-weight:bold"> ${famousPerson}</span> was stoked about the MadLib Challenge in class today. But they were having trouble coming up with <span style="text-decoration:underline ; font-weight:bold"> ${anyNumber}</span> <span style="text-decoration:underline ; font-weight:bold"> ${pluralNoun}</span> to help them with the Challenge. They figured out they should <span style="text-decoration:underline ; font-weight:bold"> ${verb}</span> to save time. Little did they know it caused them to <span style="text-decoration:underline ; font-weight:bold"> ${descriptiveAdjective}</span>. But in the end it\'s all <span style="text-decoration:underline ; font-weight:bold"> ${slangWord}</span> and they were really happy about that.`; 
 
-    var theSentence = document.getElementById('theSentence');
-    theSentence.innerHTML = results;
+    
     
         //error message for blank spaces on madlib
     if (
         famousPerson.length === 0 || anyNumber.length === 0 ||          pluralNoun.length === 0 || verb.length === 0 ||                 descriptiveAdjective.length === 0 || slangWord.length === 0
     )
         //alert message
-    {alert("PLEASE FILL OUT ALL BLANKS TO MAKE YOUR MADLIB :)");}
-
-        //if no blanks then it fills out madlib
+        {alert("PLEASE FILL OUT ALL BLANKS TO MAKE YOUR MADLIB :)");}
+    
+    //if no blanks then it fills out madlib
     else {
+        var theSentence = document.getElementById('theSentence');
+        theSentence.innerHTML = results; //sentence with results
+
         document.getElementById('famous-person').innerHTML =        famousPerson;
         document.getElementById('any-number').innerHTML = anyNumber;
         document.getElementById('plural-noun').innerHTML =  pluralNoun;
         document.getElementById('verb').innerHTML = verb;
         document.getElementById('descriptive-adjetive').innerHTML =     descriptiveAdjective;
         document.getElementById('slang-word').innerHTML = slangWord;
+        
     }
 
 }
+
+
+//start over button
+function pushit() {
+    document.getElementById('famousp').reset();
+    document.getElementById('anyn').reset();
+    document.getElementById('pluraln').reset();
+    document.getElementById('v').reset();
+    document.getElementById('descriptivea').reset();
+    document.getElementById('slangw').reset();
+    
+    document.getElementById('theSentence');
+    theSentence.innerHTML = "";
+}
+        
